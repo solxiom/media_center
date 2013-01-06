77d7d54b24fd06189217a50a3379c8f76b3ccaaa
kavan soleimanbeigi <solxiom@gmail.com>
2013-01-06 06:04:33 +0200

    some fixes for filesystem

--- src/filesystem/Main.java
+++ src/filesystem/Main.java
@@ -13,7 +13,8 @@ import java.util.Scanner;
  * @author kavan
  */
 public class Main {
-     /**
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -21,93 +22,90 @@ public class Main {
         FTPFileManager setup = new FTPFileManager();
         Scanner sc = new Scanner(System.in);
         FTPMediaFile[] fold = setup.listCategories();
-        int luku =0;
-        FTPMediaFile currentFolder=null;
+        int luku = 0;
+        FTPMediaFile currentFolder = null;
         //System.out.println(new LocalFileManager().makeDir("Downloads"));
-         //System.out.println(System.getProperty("user.dir")+"/Tools/VLC/vlc.exe");
+        //System.out.println(System.getProperty("user.dir")+"/Tools/VLC/vlc.exe");
         new MainFrame();
         Process p = null;
-       
-        while(true){
-          
-            if(currentFolder != null && currentFolder.getParentFolder() != null)
-            {
-              System.out.println(" path: " + currentFolder.getFilePath());
-              System.out.println(-1 + "  \\..");
+
+        while (true) {
+
+            if (currentFolder != null && currentFolder.getParentFolder() != null) {
+                System.out.println(" path: " + currentFolder.getFilePath());
+                System.out.println(-1 + "  \\..");
+            } else {
+                System.out.println(" path: disk1/Media");
             }
-            else
-                System.out.println(" path: disk1/Media" );
             int i = 0;
-            
+
 //            System.out.println("folder length " + fold.length);
-            for(FTPMediaFile mf : fold)
-            {
+            for (FTPMediaFile mf : fold) {
                 //System.out.println(""+fold.length);
-                System.out.println(" "+ i+" - " + mf.getMediaType()+" : "+mf.getName());
-                
+                if (mf.isMediaType(TypeX.VIDEO)) {
+                    System.out.println(" " + i + " - " + mf.getMediaType() + " : " + mf.getMediaName() + "..............[" + mf.getName() + "]");
+                } else {
+                    System.out.println(" " + i + " - " + mf.getMediaType() + " : " + mf.getMediaName());
+                }
                 i++;
             }
 
             System.out.println("anna numero : ");
             String par = sc.nextLine();
-            if(par.equalsIgnoreCase("exit")){
+            if (par.equalsIgnoreCase("exit")) {
                 System.out.println("bye..");
                 break;
             }
-            if(par.equalsIgnoreCase("c") && p != null)
+            if (par.equalsIgnoreCase("c") && p != null) {
                 p.destroy();
-            else
-                try{
-                     
-                     luku = Integer.parseInt(par);
-                   
-                    if(luku != -1 && fold[luku].getFTPFile().isDirectory())
-                    {
-                       
-                            //System.out.println(fold[luku].getFilePath());
-                        
-                       
-                                currentFolder = fold[luku];
-                                fold = fold[luku].listFTPMediaFiles();
-                        
-                                
-                                
-                    }
-                    else if(luku == -1){
-                         //if(currentFolder.getParentFolder() == null)
-                           //  System.out.println("You are in Home folder!!");
-                         if(currentFolder != null && currentFolder.getParentFolder() != null)
-                         {
+            } else {
+                try {
 
-                               fold = currentFolder.getParentFolder().listFTPMediaFiles();
-                               currentFolder = currentFolder.getParentFolder();
-                               //System.out.println(" parent " + currentFolder.getParentFolder().getMediaName() );
+                    luku = Integer.parseInt(par);
+
+                    if (luku != -1 && fold[luku].getFTPFile().isDirectory()) {
+
+                        //System.out.println(fold[luku].getFilePath());
+
+
+                        currentFolder = fold[luku];
+                        fold = fold[luku].listFTPMediaFiles();
+
+
+
+                    } else if (luku == -1) {
+                        //if(currentFolder.getParentFolder() == null)
+                        //  System.out.println("You are in Home folder!!");
+                        if (currentFolder != null && currentFolder.getParentFolder() != null) {
+
+                            fold = currentFolder.getParentFolder().listFTPMediaFiles();
+                            currentFolder = currentFolder.getParentFolder();
+                            //System.out.println(" parent " + currentFolder.getParentFolder().getMediaName() );
+                        } else {
+                            System.out.println(" This is your root folder!!");
                         }
-                         else
-                             System.out.println(" This is your root folder!!" );
-                         
-                             
-                    }
 
-                    else{
-                        System.out.println("Opening "+fold[luku].getMediaType()+" file...");
-                        
+
+                    } else {
+                        System.out.println("Opening " + fold[luku].getMediaType() + " file...");
+
                         p = new Controller().openMediaFile(fold[luku]);
-                        
-                       // p.destroy();
+
+                        // p.destroy();
                     }
-            }catch(Exception e){
-                           System.out.println(  e);
-                           break;
+                } catch (Exception e) {
+                    System.out.println(e);
+                    break;
+                }
             }
 
 
         }
 
         Controller co = new Controller();
-        System.out.println("Opening "+ System.getProperty("user.dir") );
-              
-     
+        System.out.println("Opening " + System.getProperty("user.dir"));
+
+
 
 
         // TODO code application logic here
